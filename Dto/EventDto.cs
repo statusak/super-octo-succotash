3f51c292e66d181c54b119921cd6cc651afb5ee9
user <user@mail.mail>
2026-04-02 13:29:05 -0700

    Rename folder

---
+++ Dto/EventDto.cs
@@ -0,0 +1,19 @@
+﻿using CSCourse.DTO;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSCourse.Dto
+{
+    public class EventDto
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "StartAt is required.")]
+        public required DateTime StartAt { get; set; }
+        
+        [Required(ErrorMessage = "EndAt is required.")]
+        [DateTimeValidator(ErrorMessage = "EndAt must be later than StartAt.")]
+        public required DateTime EndAt { get; set; }
+    }
+}
