f4ed6b76d1bd8cfd05ee7c78a636e87a0614a10b
user <user@mail.mail>
2026-04-02 11:57:55 -0700

    Crate interface IEventService

---
+++ IEventService.cs
@@ -0,0 +1,11 @@
+﻿namespace CSCourse
+{
+    public interface IEventService
+    {
+        List<Event> GetAll();
+        Event GetEventById(int id);
+        void CreateEvent(Event @event);
+        void UpdateEvent(int id, Event @event);
+        void DeleteEvent(int id);
+    }
+}
