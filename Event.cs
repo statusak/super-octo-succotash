aed9bce5f340a43af3e88c4e90fb3ad8fa63b071
user <user@mail.mail>
2026-04-02 11:49:00 -0700

    Create model Event

---
+++ Event.cs
@@ -0,0 +1,28 @@
+﻿namespace CSCourse
+{
+    public class Event
+    {
+        public required int Id { get; set; }
+        public required string Title { get; set; }
+        private string Description { get; set; }
+        public required DateTime StartAt { get; set; }
+        public required DateTime EndAt { get; set; }
+
+        public Event(int id, string title, string description, DateTime startAt, DateTime endAt)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            StartAt = startAt;
+            EndAt = endAt;
+        }
+        public Event(int id, string title, DateTime startAt, DateTime endAt)
+        {
+            Id = id;
+            Title = title;
+            Description = "";
+            StartAt = startAt;
+            EndAt = endAt;
+        }
+    }
+}
