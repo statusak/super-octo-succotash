c73b84dc87dfb0e5b2eaf6a16589e84cb87a4e6f
user <user@mail.mail>
2026-04-15 14:55:38 -0700

    Создан тест для валидатора StartAt EndAr

--- EventServiceTest/UnitEventServiceTest.cs
+++ EventServiceTest/UnitEventServiceTest.cs
@@ -1,8 +1,12 @@
 ﻿using CSCourse.Controllers;
 using CSCourse.Models;
 using CSCourse.Services;
+using CSCourse.Validators;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Moq;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventServiceTest
 {
@@ -16,6 +20,8 @@ namespace EventServiceTest
         {
             _mockEventService = new Mock<IEventService>();
             _controller = new EventsController(_mockEventService.Object);
+
+            _mockEventService.Setup(s => s.CreateEvent(It.IsAny<Event>()));
         }
 
 
@@ -30,12 +36,55 @@ namespace EventServiceTest
                 EndAt = DateTime.Now.AddHours(2)
             };
 
-            _mockEventService.Setup(s => s.CreateEvent(It.IsAny<Event>()));
+            
 
             var result = _controller.Post(validDto) as CreatedResult;
 
             Assert.NotNull(result);
             Assert.Equal(201, result.StatusCode);
+        }
+
+        [Fact]
+        public void DateTimeValidator_EndBeforeStart_ReturnsError()
+        {
+            // Arrange
+            var dto = new EventDto
+            {
+                Title = "Тест",
+                StartAt = DateTime.Now.AddHours(2),
+                EndAt = DateTime.Now.AddHours(1)
+            };
+
+            var validator = new DateTimeValidator { ErrorMessage = "EndAt must be later than StartAt." };
+            var validationContext = new ValidationContext(dto);
+
+            // Act
+            var result = validator.GetValidationResult(dto.EndAt, validationContext);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("EndAt must be later than StartAt.", result.ErrorMessage);
+        }
+
+        [Fact]
+        public void DateTimeValidator_StartBeforeEnd_ReturnsSuccess()
+        {
+            // Arrange
+            var dto = new EventDto
+            {
+                Title = "Тест",
+                StartAt = DateTime.Now.AddHours(1),
+                EndAt = DateTime.Now.AddHours(2)
+            };
+
+            var validator = new DateTimeValidator { ErrorMessage = "EndAt must be later than StartAt." };
+            var validationContext = new ValidationContext(dto);
+
+            // Act
+            var result = validator.GetValidationResult(dto.EndAt, validationContext);
+
+            // Assert
+            Assert.Null(result);
         }
     }
 }
