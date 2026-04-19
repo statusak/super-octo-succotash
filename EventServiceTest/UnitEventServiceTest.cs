c6b1c8ea62fde08f33cc8e51f89735cacb9e77a9
user <user@mail.mail>
2026-04-19 07:23:42 -0700

    Разделение тестов для контроллера и для валидации

--- EventServiceTest/UnitEventServiceTest.cs
+++ EventServiceTest/UnitEventServiceTest.cs
@@ -2,7 +2,6 @@
 using CSCourse.Models;
 using CSCourse.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
 
 namespace EventServiceTest
 {
@@ -52,56 +51,7 @@ namespace EventServiceTest
             Assert.Equal(201, result.StatusCode);
         }
 
-        [Fact]
-        public void DateTimeValidator_EndBeforeStart_ReturnsError()
-        {
-            var invalidDto = new EventDto
-            {
-                Title = "Тестовая конференция",
-                Description = "Описание мероприятия",
-                StartAt = DateTime.Now.AddHours(2),
-                EndAt = DateTime.Now.AddHours(1)
-            };
-
-            var validationResults = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(
-                invalidDto,
-                new ValidationContext(invalidDto),
-                validationResults,
-                true
-            );
-
-            Assert.False(isValid);
-            Assert.NotEmpty(validationResults);
-            Assert.Contains(
-                validationResults,
-                vr => vr.ErrorMessage == "EndAt must be later than StartAt."
-            );
-
-        }
-
-        [Fact]
-        public void DateTimeValidator_StartBeforeEnd_ReturnsSuccess()
-        {
-            var invalidDto = new EventDto
-            {
-                Title = "Тестовая конференция",
-                Description = "Описание мероприятия",
-                StartAt = DateTime.Now.AddHours(1),
-                EndAt = DateTime.Now.AddHours(2)
-            };
-
-            var validationResults = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(
-                invalidDto,
-                new ValidationContext(invalidDto),
-                validationResults,
-                true
-            );
-
-            Assert.True(isValid);
-            Assert.Empty(validationResults);
-        }
+        
 
         [Fact]
         public void GetAll_WithValidData_ReturnsOkResultWithPaginatedEvents()
