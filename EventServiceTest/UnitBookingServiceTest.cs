993c99d58000bc383c249ada8935ddecfa3b3313
user <user@mail.mail>
2026-04-26 14:27:55 -0700

    Тест для отобрадения корректной информации для созданного

--- EventServiceTest/UnitBookingServiceTest.cs
+++ EventServiceTest/UnitBookingServiceTest.cs
@@ -13,7 +13,8 @@ namespace EventServiceTest
     public class UnitBookingServiceTest
     {
         private readonly EventMemoryService _eventService;
-        private readonly EventsController _controller;
+        private readonly EventsController _eventsController;
+        private readonly BookingsController _bookingsController;
 
         public UnitBookingServiceTest()
         {
@@ -21,7 +22,8 @@ namespace EventServiceTest
             var bookingService = new BookingMemoryService();
             var bookingTaskQueue = new InMemoryBookingTaskQueue();
             var logger = NullLogger<EventsController>.Instance;
-            _controller = new EventsController(_eventService, bookingService, bookingTaskQueue, logger);
+            _eventsController = new EventsController(_eventService, bookingService, bookingTaskQueue, logger);
+            _bookingsController = new BookingsController(bookingService);
         }
 
         [Fact]
@@ -35,7 +37,7 @@ namespace EventServiceTest
                 EndAt = DateTime.Now.AddHours(2)
             };
 
-            var resultCreateEvent = _controller.Post(validDto).Result as CreatedAtActionResult;
+            var resultCreateEvent = _eventsController.Post(validDto).Result as CreatedAtActionResult;
             
             Assert.NotNull(resultCreateEvent);
             Assert.Equal(201, resultCreateEvent.StatusCode);
@@ -43,7 +45,7 @@ namespace EventServiceTest
             var @event = resultCreateEvent.Value as Event;
             Assert.NotNull(@event);
 
-            var resultCreateBooking = (await _controller.CreateBooking(@event.Id)) as AcceptedAtActionResult;
+            var resultCreateBooking = (await _eventsController.CreateBooking(@event.Id)) as AcceptedAtActionResult;
 
             Assert.NotNull(resultCreateBooking);
             Assert.Equal(202, resultCreateBooking.StatusCode);
@@ -65,7 +67,7 @@ namespace EventServiceTest
                 EndAt = DateTime.Now.AddHours(2)
             };
 
-            var resultCreateEvent = _controller.Post(validDto).Result as CreatedAtActionResult;
+            var resultCreateEvent = _eventsController.Post(validDto).Result as CreatedAtActionResult;
 
             Assert.NotNull(resultCreateEvent);
             Assert.Equal(201, resultCreateEvent.StatusCode);
@@ -77,7 +79,7 @@ namespace EventServiceTest
 
             for (int i = 0; i < 10; i++)
             {
-                var resultCreateBooking = (await _controller.CreateBooking(@event.Id)) as AcceptedAtActionResult;
+                var resultCreateBooking = (await _eventsController.CreateBooking(@event.Id)) as AcceptedAtActionResult;
 
                 Assert.NotNull(resultCreateBooking);
                 Assert.Equal(202, resultCreateBooking.StatusCode);
@@ -89,6 +91,45 @@ namespace EventServiceTest
                 Assert.DoesNotContain(booking.Id, CreatedBookings);
                 CreatedBookings.Add(booking.Id);
             }
+        }
+
+        [Fact]
+        public async Task BookingService_CheckInfoBooking_Success()
+        {
+            var validDto = new EventDto
+            {
+                Title = "Тестовая конференция",
+                Description = "Описание мероприятия",
+                StartAt = DateTime.Now.AddHours(1),
+                EndAt = DateTime.Now.AddHours(2)
+            };
+
+            var resultCreateEvent = _eventsController.Post(validDto).Result as CreatedAtActionResult;
+
+            Assert.NotNull(resultCreateEvent);
+            Assert.Equal(201, resultCreateEvent.StatusCode);
+
+            var @event = resultCreateEvent.Value as Event;
+            Assert.NotNull(@event);
+
+            var resultCreateBooking = (await _eventsController.CreateBooking(@event.Id)) as AcceptedAtActionResult;
+
+            Assert.NotNull(resultCreateBooking);
+            Assert.Equal(202, resultCreateBooking.StatusCode);
+
+            var bookingCreate = resultCreateBooking.Value as Booking;
+            Assert.NotNull(bookingCreate);
+            Assert.Equal(BookingStatus.Pending, bookingCreate.Status);
+            Assert.Equal(@event.Id, bookingCreate.EventId);
+
+            var resultInfoBooking = (await _bookingsController.GetById(@event.Id)) as AcceptedAtActionResult;
+
+            Assert.NotNull(resultCreateBooking);
+            Assert.Equal(202, resultCreateBooking.StatusCode);
+
+            var bookingInfo = resultCreateBooking.Value as Booking;
+            Assert.NotNull(bookingInfo);
+            Assert.Equal(bookingCreate, bookingInfo);
         }
     }
 }
