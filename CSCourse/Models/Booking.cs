674af89dbd055e7592ffff5f554163925ecbfc1c
user <user@mail.mail>
2026-04-26 07:41:15 -0700

    Создана модель Booking

---
+++ CSCourse/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿namespace CSCourse.Models
+{
+    public enum BookingStatus
+    {
+        Pending,
+        Confirmed,
+        Rejected
+    }
+    public class Booking
+    {
+        public required Guid Id { get; set; }
+        public required Guid EventId { get; set; }
+        public required BookingStatus Status { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public required DateTime? ProcessedAt { get; set; }
+    }
+}
