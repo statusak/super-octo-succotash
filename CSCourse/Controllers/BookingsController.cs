4d20c5ea0eb123ad187ea7771f4b83966ed9097a
user <user@mail.mail>
2026-04-26 12:38:00 -0700

    Добавлен роут GetById для бронирования

--- CSCourse/Controllers/BookingsController.cs
+++ CSCourse/Controllers/BookingsController.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CSCourse.Interfaces;
+using CSCourse.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CSCourse.Controllers
 {
 
     [ApiController]
     [Route("/[controller]")]
-    public class BookingsController : ControllerBase
+    public class BookingsController(IBookingService _bookingService) : ControllerBase
     {
         [HttpGet("{index:Guid}")]
-        public ActionResult GetById(Guid index)
+        public async Task<ActionResult> GetById(Guid index)
         {
-            return Ok();
+            Booking? booking = await _bookingService.GetBookingByIdAsync(index);
+            if (booking != null) 
+            {
+                return Ok(booking);
+            }
+            return NotFound($"Booking with index {index} not found");
         }
     }
 }
