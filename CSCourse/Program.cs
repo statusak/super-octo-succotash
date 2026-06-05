56d1863a8146062f1437e7069593fb0882090201
statusak <statusak@mail.mail>
2026-06-05 18:33:11 -0400

    Изменение БД через Миграцию

--- CSCourse/Program.cs
+++ CSCourse/Program.cs
@@ -38,8 +38,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
-}
+    db.Database.Migrate();
+} 
 
 app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
