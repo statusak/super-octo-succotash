9a10365eeecc49a74b332f31f4117fddcd70d01a
statusak <statusak@mail.mail>
2026-08-24 17:12:21 -0400

    добавлена миграция для Event Table

---
+++ Events.Service/Infrastructure/Migrations/20260824211204_InitEventTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Events.Service.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitEventTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Description = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: true),
+                    TotalSeats = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    AvailableSeats = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    StartAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_events", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "events");
+        }
+    }
+}
