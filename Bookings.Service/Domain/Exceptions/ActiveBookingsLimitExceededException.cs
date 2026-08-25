f364243471221a2db101e1616f4351a2f285f14c
statusak <statusak@mail.mail>
2026-08-25 13:50:37 -0400

    Убраны не нужные exceptions

--- Bookings.Service/Domain/Exceptions/ActiveBookingsLimitExceededException.cs
+++
@@ -1,9 +0,0 @@
-namespace Bookings.Service.Domain.Exceptions;
-
-/// <summary>
-/// Выбрасывается, когда у пользователя превышен лимит активных броней.
-/// </summary>
-public class ActiveBookingsLimitExceededException : Exception
-{
-    public ActiveBookingsLimitExceededException(string message) : base(message) { }
-}
--- Bookings.Service/Domain/Exceptions/NoAvailableSeatsException.cs
+++
@@ -1,6 +0,0 @@
-namespace Bookings.Service.Domain.Exceptions;
-
-public class NoAvailableSeatsException : Exception
-{
-    public NoAvailableSeatsException(string message) : base(message) { }
-}
