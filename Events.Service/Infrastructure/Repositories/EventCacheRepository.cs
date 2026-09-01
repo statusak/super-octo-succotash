4f550c54f981c0e7a47c3626c38048c08d70dfba
statusak <mail@mail.mail>
2026-09-01 16:03:04 -0400

    Добавлена обработка ошибок, связанная с Redis

--- Events.Service/Infrastructure/Repositories/EventCacheRepository.cs
+++ Events.Service/Infrastructure/Repositories/EventCacheRepository.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using Events.Service.Application.Interfaces;
 using Events.Service.Domain.Models;
+using Microsoft.Extensions.Logging;
 using StackExchange.Redis;
 
 namespace Events.Service.Infrastructure.Repositories;
@@ -9,13 +10,15 @@ public class EventCacheRepository : IEventCacheRepository
 {
     private readonly IDatabase _redis;
     private readonly IEventRepository _repository;
+    private readonly ILogger _logger;
     private static readonly TimeSpan ExpiryEventById = TimeSpan.FromMinutes(10);
     private static readonly TimeSpan ExpiryTop10Events = TimeSpan.FromMinutes(1);
 
-    public EventCacheRepository(IConnectionMultiplexer connection, IEventRepository repository)
+    public EventCacheRepository(IConnectionMultiplexer connection, IEventRepository repository, ILogger logger)
     {
         _redis = connection.GetDatabase();
         _repository = repository;
+        _logger = logger;
     }
 
 
@@ -32,8 +35,14 @@ public class EventCacheRepository : IEventCacheRepository
             return null;
 
         var serialized = JsonSerializer.Serialize(@event);
-        await _redis.StringSetAsync(key, serialized, ExpiryEventById);
 
+
+        try{
+            await _redis.StringSetAsync(key, serialized, ExpiryEventById);
+        } catch (RedisException ex)
+        {
+            _logger.LogError(ex, $"Ошибка установки ключа {key} в Redis");
+        }
         return @event;
     }
 
@@ -41,19 +50,32 @@ public class EventCacheRepository : IEventCacheRepository
     {
         const string cacheKey = "events:top10";
 
-        var cached = await _redis.StringGetAsync(cacheKey);
-        if (cached.HasValue)
+        try
         {
-            return JsonSerializer.Deserialize<List<Event>>(cached.ToString())!;
+            var cached = await _redis.StringGetAsync(cacheKey);
+            if (cached.HasValue)
+            {
+                return JsonSerializer.Deserialize<List<Event>>(cached.ToString())!;
+            }
+        }
+        catch (RedisException ex)
+        {
+            _logger.LogError(ex, $"Ошибка получения ключа {cacheKey} из Redis при получении top10");
         }
 
         var events = await _repository.GetTop10Async();
 
-        await _redis.StringSetAsync(
-            cacheKey,
-            JsonSerializer.Serialize(events),
-            ExpiryTop10Events
-        );
+        try
+        {
+            await _redis.StringSetAsync(
+                cacheKey,
+                JsonSerializer.Serialize(events),
+                ExpiryTop10Events
+            );
+        } catch (RedisException ex)
+        {
+            _logger.LogError(ex, $"Ошибка установки ключа {cacheKey} в Redis");
+        }
 
         return events;
     }
